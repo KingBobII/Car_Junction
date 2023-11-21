6124ee0f6c54c2080c1ff4e902ef3b4a22ffad07
Kananelo Mosia <kananelomosia16@gmail.com>
2023-11-21 16:10:47 +0200

    created a bookings model and controller and coded the actions for it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def show
+    @car = Car.find(params[:id])
+    @booking = Booking.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "/cars/:id", to: "pages#show", as: :show_cars
+  get "/cars/:user_id/:car_id/bookings/new", to: "bookings#new"
+  post "/cars/:user_id/:car_id/bookings", to: "bookings#create"
+  delete "/cars/:user_id/:car_id/bookings/:id", to: "bookings#destroy"
+  get "/cars/:user_id/bookings", to: "bookings#index", as: :display_bookings
+  patch "/cars/:user_id/bookings/accept", to: "bookings#accept"
+  patch "/cars/:user_id/bookings/decline", to: "bookings#decline"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
